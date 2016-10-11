7a617e659a993dd76d47c30b80f40f44f43c6760
Mark Hendrickson <markmhendrickson@gmail.com>
2016-10-11 15:54:01 +0200

    Add deploy task to Gruntfile; downgrade Grunt
    And remove node_modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,11 +20,23 @@ module.exports = function(grunt) {
       }
     },
     replace: {
-      build: {
+      buildDev: {
         options: {
           patterns: [{
             match: 'SYNC_WEB_SERVER_HOST',
             replacement: process.env.SYNC_WEB_STUB_SERVER_HOST
+          }]
+        },
+        files: [{
+          src: 'build/assets/scripts.js',
+          dest: 'build/assets/scripts.js'
+        }]
+      },
+      buildProd: {
+        options: {
+          patterns: [{
+            match: 'SYNC_WEB_SERVER_HOST',
+            replacement: process.env.SYNC_WEB_STUB_DEPLOY_SERVER_HOST
           }]
         },
         files: [{
@@ -43,21 +55,66 @@ module.exports = function(grunt) {
         files: ['app/*'],
         tasks: ['build']
       }
+    },
+    rsync: {
+      deploy: {
+        options: {
+          exclude: [
+            ".DS_Store",
+            ".git",
+            "node_modules",
+            "*.sublime*"
+          ],
+          recursive: true,
+          src: './',
+          dest: process.env.SYNC_WEB_STUB_DEPLOY_HOST_DIR,
+          host: process.env.SYNC_WEB_STUB_DEPLOY_HOST_USERNAME + '@' + process.env.SYNC_WEB_STUB_DEPLOY_HOST
+        }
+      }
+    },
+    sshexec: {
+      options: {
+        host: process.env.SYNC_WEB_STUB_DEPLOY_HOST,
+        port: 22,
+        username: process.env.SYNC_WEB_STUB_DEPLOY_HOST_USERNAME,
+        agent: process.env.SSH_AUTH_SOCK
+      },
+      npmInstall: {
+        command: 'cd ' + process.env.SYNC_WEB_STUB_DEPLOY_HOST_DIR + ' && npm install --production'
+      },
+      foreverRestartAll: {
+        command: 'cd ' + process.env.SYNC_WEB_STUB_DEPLOY_HOST_DIR + ' && forever restart server.js'
+      }
     }
   });
 
   require('load-grunt-tasks')(grunt);
 
-  // Build app from source files
-  grunt.registerTask('build', [
+  // Build app from source files for dev environment
+  grunt.registerTask('buildDev', [
     'clean:build',
     'copy:build',
-    'replace:build'
+    'replace:buildDev'
+  ]);
+
+  // Build app from source files for production environment
+  grunt.registerTask('buildProd', [
+    'clean:build',
+    'copy:build',
+    'replace:buildProd'
   ]);
 
   // Build app and run local web server for development
   grunt.registerTask('dev', [
-    'build',
+    'buildDev',
     'concurrent:build'
+  ]);
+
+  // Deploy to host
+  grunt.registerTask('deploy', [
+    'buildProd',
+    'rsync:deploy',
+    'sshexec:npmInstall',
+    'sshexec:foreverRestartAll'
   ]);
 };
